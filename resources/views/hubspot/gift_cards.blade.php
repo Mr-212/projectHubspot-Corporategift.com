8284a7b8e4d1de75789834147256b072af409b52
Ali Raza <ali.raza@techloyce.com>
2021-04-05 15:58:53 +0500

    updated

--- resources/views/hubspot/gift_cards.blade.php
+++ resources/views/hubspot/gift_cards.blade.php
@@ -3,25 +3,27 @@
     <div class="container">
    <div class="row">
 
-
         @if(isset($gift_products) && count($gift_products) > 0)
             @foreach($gift_products as $card)
                 <div class="col-md-4 col-sm-4">
-
 
                 <div class="card" style="width: 18rem;">
                     <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
-                        <p class="card-text overflow-auto" >{{$card['description']}}</p>
+
+                        <div class="">
+                            <p class="card-text overflow-scroll"  style="height: 80px; overflow-y: scroll">{{ strip_tags($card['description'])}}</p>
+
+                        </div>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Status: <strong>In Process</strong></li>
+                    {{--<ul class="list-group list-group-flush">--}}
+                        {{--<li class="list-group-item">Status: <strong>In Process</strong></li>--}}
                         {{--<li class="list-group-item">Dapibus ac facilisis in</li>--}}
                         {{--<li class="list-group-item">Vestibulum at eros</li>--}}
-                    </ul>
+                    {{--</ul>--}}
                     <div class="card-body">
-                        <a href="{{url('/').'/post_hubspot_send_gift_request'}}" class="card-link">Send</a>
+                        <a href="{{url('/').'/get_hupspot_send_gift_request?product_id='.$card["id"].'&email='.$email}}" class="card-link">Send</a>
                         {{--<a href="#" class="card-link">View</a>--}}
                     </div>
                 </div>
