d0a33c78387dafc6ddd4f97f2bd3cd9d5e2ff899
Ali Raza <ali.raza@techloyce.com>
2021-04-05 16:28:03 +0500

    updated made .

--- resources/views/hubspot/gift_cards.blade.php
+++ resources/views/hubspot/gift_cards.blade.php
@@ -10,19 +10,15 @@
                 <div class="card" style="width: 18rem;">
                     <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">{{$card['name']}}</h5>
+                        <p class="card-text overflow-scroll"  style="height: 80px; overflow-y: scroll">{{ strip_tags($card['description'])}}</p>
 
-                        <div class="">
-                            <p class="card-text overflow-scroll"  style="height: 80px; overflow-y: scroll">{{ strip_tags($card['description'])}}</p>
-
-                        </div>
                     </div>
-                    {{--<ul class="list-group list-group-flush">--}}
-                        {{--<li class="list-group-item">Status: <strong>In Process</strong></li>--}}
-                        {{--<li class="list-group-item">Dapibus ac facilisis in</li>--}}
-                        {{--<li class="list-group-item">Vestibulum at eros</li>--}}
-                    {{--</ul>--}}
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Price: <strong>{{$card['price']}}</strong></li>
+                    </ul>
                     <div class="card-footer">
+                        {{--<p>{{$card['price']}}</p>--}}
                         {{--<a class="card-link" href="{{url('/').'/get_hupspot_send_gift_request?product_id='.$card["id"].'&email='.$email}}" >Send</a>--}}
                         <a class="btn btn-primary float-right" href="{{url('/').'/get_hupspot_send_gift_request?product_id='.$card["id"].'&email='.$email}}" >Send</a>
                         {{--<a href="#" class="card-link">View</a>--}}
