66e76eb017740d353f8ee84b61dd6accdf660d4c
Ali Raza <ali.raza@techloyce.com>
2021-04-05 13:33:51 +0500

    updated

--- resources/views/hubspot/gift_cards.blade.php
+++ resources/views/hubspot/gift_cards.blade.php
@@ -1,58 +1,35 @@
 @extends('layouts.layoutiframe')
 @section('content')
     <div class="container">
-        <div class="card w-75">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Button</a>
-            </div>
-        </div>
+   <div class="row">
 
-        <div class="card w-50">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Button</a>
-            </div>
-        </div>
+
         @if(isset($gift_products) && count($gift_products) > 0)
             @foreach($gift_products as $card)
-            <div class="card w-75">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
-                </div>
-            </div>
+                <div class="col-md-4 col-sm-4">
 
-            <div class="card w-50">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Button</a>
-                </div>
-            </div>
 
-            {{--<form id="contact" action="{{url('/').'/post_hubspot_send_gift_request'}}" method="post">--}}
-                {{--@csrf--}}
-                {{--<h3>Send Gift</h3>--}}
-                {{--<fieldset>--}}
-                    {{--<input placeholder="Subject" type="text" name="subject" tabindex="1" required autofocus>--}}
-                {{--</fieldset>--}}
-                {{--<fieldset>--}}
-                    {{--<input placeholder="Email Address" type="email" name="email" value="{{$email}}" tabindex="2" disabled>--}}
-                {{--</fieldset>--}}
-                {{--<fieldset>--}}
-                    {{--<textarea placeholder="Type your message here...." name="message" tabindex="5" required></textarea>--}}
-                {{--</fieldset>--}}
-                {{--<fieldset>--}}
-                    {{--<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>--}}
-                {{--</fieldset>--}}
-    {{----}}
-            {{--</form>--}}
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text overflow-auto" >{{$card['description']}}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Status: <strong>In Process</strong></li>
+                        {{--<li class="list-group-item">Dapibus ac facilisis in</li>--}}
+                        {{--<li class="list-group-item">Vestibulum at eros</li>--}}
+                    </ul>
+                    <div class="card-body">
+                        <a href="{{url('/').'/post_hubspot_send_gift_request'}}" class="card-link">Send</a>
+                        {{--<a href="#" class="card-link">View</a>--}}
+                    </div>
+                </div>
+                </div>
+
             @endforeach
 
          @endif
+   </div>
     </div>
 @stop
