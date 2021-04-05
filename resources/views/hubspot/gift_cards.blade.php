d6fba7ceb9d07a1fa376a94205147b3111f02717
Ali Raza <ali.raza@techloyce.com>
2021-04-05 16:22:24 +0500

    updated made .

--- resources/views/hubspot/gift_cards.blade.php
+++ resources/views/hubspot/gift_cards.blade.php
@@ -5,7 +5,7 @@
 
         @if(isset($gift_products) && count($gift_products) > 0)
             @foreach($gift_products as $card)
-                <div class="col-md-4 col-sm-4">
+                <div class="col-md-4 col-sm-4 mt-5" >
 
                 <div class="card" style="width: 18rem;">
                     <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
