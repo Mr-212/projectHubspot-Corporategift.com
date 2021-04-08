5f6b9f2e7f13ac4e18dd9ca3d20eb0b9b4de93bf
Ali Raza <ali.raza@techloyce.com>
2021-04-08 15:33:22 +0500

    updated

--- app/Http/Controllers/HupSpotServiceController.php
+++ app/Http/Controllers/HupSpotServiceController.php
@@ -265,8 +265,9 @@ class HupSpotServiceController extends Controller
 
 
 //        Log::info(@$request->getMethod());
-
+        $name = null;
         $email =  @$request->get('email');
+        if($request->has('firstname') && $request->has('lastname'))
         $name  =  @$request->get('firstname'). ' '.@$request->get('lastname');
         //$CorporateGiftGet = $this->getGiftProducts();
         $index = 0;
@@ -328,7 +329,7 @@ class HupSpotServiceController extends Controller
         $gift_arr['primaryAction']['type']='IFRAME';
         $gift_arr['primaryAction']['width']=1100;
         $gift_arr['primaryAction']['height']=748;
-        $gift_arr['primaryAction']['uri'] = url('/')."/get_all_gift_products?identifier={$identifier}&email={$email}&name={$name}";;
+        $gift_arr['primaryAction']['uri'] = url('/')."/get_all_gift_products?identifier={$identifier}&email={$email}&name={$name}";
         $gift_arr['primaryAction']['label']='View Gift Products';
 
 
