0798172ff840d12fd4aea0abece80ae2af48f806
Ali Raza <ali.raza@techloyce.com>
2021-04-08 18:45:43 +0500

    updated

--- app/Http/Controllers/HupSpotServiceController.php
+++ app/Http/Controllers/HupSpotServiceController.php
@@ -547,7 +547,7 @@ class HupSpotServiceController extends Controller
          //dd($request->get('data'));
          $form = $request->get('data');
 
-         $return = ['status'=>false,'data'=>($form)];
+         $return = ['status'=>false,'data'=>($request->all())];
 
          $identifier = @$form['identifier'];
          $subject = @$form['subject'];
