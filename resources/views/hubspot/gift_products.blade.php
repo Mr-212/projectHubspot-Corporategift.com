b1679be79bfb2ab945efc88728dc24bf66d6fded
Ali Raza <ali.raza@techloyce.com>
2021-04-08 19:04:49 +0500

    updated

--- resources/views/hubspot/gift_products.blade.php
+++ resources/views/hubspot/gift_products.blade.php
@@ -66,14 +66,13 @@
 
                            </div>
                            <div class="modal-footer">
-                               <button class="btn btn-info" id="sending_button" type="button" style="display: none" disabled>
-                                   <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                                   Sending...
-                               </button>
-
-
                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                <button type="button" id="send_gift_button" data-id="{{$card['data']['id']}}" data-url="{{url('/')."/post_hubspot_send_gift_request?product_id={$card['data']['id']}&name={$name}"}}" class="btn btn-primary">Send</button>
+
+                               <button class="btn btn-info" id="sending_button" type="button" style="display: none" disabled>
+                                   <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                   <span class="sr-only">Sending...</span>
+                               </button>
                            </div>
                            </form>
                        </div>
@@ -128,8 +127,8 @@
             }
         }).done(function(data) {
 
-                    console.log(data );
-            _this.hide();
+            console.log(data );
+            _this.show();
             $('#sending_button').hide();
 
         });
