c5b401281babbcd4c72da2df24a393b2dafba463
Ali Raza <ali.raza@techloyce.com>
2021-04-14 12:08:18 +0500

    updated

--- app/Http/Controllers/HupSpotServiceController.php
+++ app/Http/Controllers/HupSpotServiceController.php
@@ -579,7 +579,7 @@ class HupSpotServiceController extends Controller
      }
 
      public function post_hubspot_send_gift_request(Request $request){
-         //dd(session('object'));
+         dd(cache()->get('app'));
          $form = $request->all();
          dd($request->all());
          $return = ['status'=>false,'data'=>($request->all())];
