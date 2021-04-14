0e5fb799d6c8e0941bdd8292cec4d6cef3ac4dad
Ali Raza <ali.raza@techloyce.com>
2021-04-14 11:23:40 +0500

    updated

--- app/Http/Controllers/HupSpotServiceController.php
+++ app/Http/Controllers/HupSpotServiceController.php
@@ -551,7 +551,7 @@ class HupSpotServiceController extends Controller
 
 
      public function get_all_gift_products(Request $request){
-         //dd(session('object_id'));
+         dd(session('object_id'));
          $params = $request->get('params');
 
          //session()->put('object',123);
