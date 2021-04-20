c63d6379075c8c71d6742e3d49779843def92174
Ali Raza <ali.raza@techloyce.com>
2021-04-20 17:06:09 +0500

    updated

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+        {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">--}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        @stack('styles')
+
+    </head>
+    <body>
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+
+<footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    @stack('scripts')
+</footer>
+</html>
