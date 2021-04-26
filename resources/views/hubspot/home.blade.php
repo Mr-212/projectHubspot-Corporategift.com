c8fc8587d53900f956853ffbd98a501e2589e22b
Ali Raza <ali.raza@techloyce.com>
2021-04-26 12:34:43 +0500

    updated .

--- resources/views/hubspot/home.blade.php
+++ resources/views/hubspot/home.blade.php
@@ -30,7 +30,7 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 @if(isset(auth()->user()->app->unique_app_id))
-                                <h3>App : {{ auth()->user()->app->unique_app_id}}</h3>
+                                    <h3>App ID: {{ auth()->user()->app->unique_app_id}}</h3>
                                 @endif
                             </div>
                             <div class="ml-12">
@@ -43,7 +43,7 @@
                 </div>
             </div>
 
-            <div class="col-md-4 float-right max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="col-md-4 float-right max-w-6xl mx-auto sm:px-6 lg:px-8 {{ isset( auth()->user()->app->unique_app_id)?:'':'disabled' }}" >
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-1">
                         <div class="p-6">
