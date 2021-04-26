3c07ac6c91731dcfde2f349c1186d8b6a879661d
Ali Raza <ali.raza@techloyce.com>
2021-04-26 14:50:58 +0500

    updated .

--- resources/views/hubspot/home.blade.php
+++ resources/views/hubspot/home.blade.php
@@ -12,9 +12,10 @@
         @if (Route::has('login'))
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <nav class="navbar navbar-expand-lg  bg-light shadow sm:rounded-lg">
+              
+              
                 @auth
                 <a href="{{ url('/Dashboard') }}" class="nav-item nav-link text-sm text-gray-700 underline disabled" >Dashboard</a>
-                <a href="{{ url('/auth/logout') }}" class="nav-item nav-link text-sm text-gray-700 underline">Logout</a>
                 @else
                 <a href="{{ route('login') }}" class="nav-item nav-link text-sm text-gray-700 underline">Log in</a>
 
@@ -22,7 +23,18 @@
                                 <a href="{{ route('register') }}" class="nav-item nav-link ml-4 text-sm text-gray-700 underline">Register</a>
                     @endif
                 @endauth
-                 
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="{{ url('/auth/logout') }}" class="nav-item nav-link text-sm text-gray-700 underline">Logout</a>
+                    </li>
+                    {{-- <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">            Dropdown on Right</a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action with a lot of text inside of an item</a>
+                      </div>
+                    </li> --}}
+                  </ul> 
                 </div>
               </nav>
         @endif      
