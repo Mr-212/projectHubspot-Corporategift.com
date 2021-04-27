164d6c492fa8ed405a3bc609eaa19260a07e27bf
Ali Raza <ali.raza@techloyce.com>
2021-04-27 10:19:19 +0500

    updated made .

--- resources/views/hubspot/home.blade.php
+++ resources/views/hubspot/home.blade.php
@@ -65,7 +65,7 @@
                                 <form  action="{{url('/').'/post_corporate_gift_token'}}" method="post">
                                     @csrf
                                     <input type="hidden" name="identifier" value="{{auth()->user()->app->identifier}}">
-                                    <div class="form-group">
+                                    <div class="row col form-group">
                                         <div class="col-md-12">
                                             <label for="staticEmail" class=""><strong>Corporate Gift Token</strong></label>
                                         </div>
@@ -73,14 +73,13 @@
                                         <div class="col-md-12">
                                             <input type="text"  class="form-control form-control" id="staticEmail" name="corporate_gift_token" value="{{ auth()->user()->app->corporate_gift_token }}" required>
                                         </div>
-                                        <div class="col-md-12">
+                                        <div class="col-md-12 my-4">
                                             <button type="submit"  class="btn btn-primary float-right" id="" value="Submit">Update</button>
                                         </div>
                                     </div>
                                    
                                 </form>
                             </div>
-                           
                         </div>
                         @endif
                     </div>
