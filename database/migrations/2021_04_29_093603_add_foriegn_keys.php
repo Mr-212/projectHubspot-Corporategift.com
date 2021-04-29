d718085baf018d5887dc763f184ac1b757615dfa
Ali Raza <ali.raza@techloyce.com>
2021-04-29 15:00:42 +0500

    updated .

--- database/migrations/2021_04_29_093603_add_foriegn_keys.php
+++ database/migrations/2021_04_29_093603_add_foriegn_keys.php
@@ -13,12 +13,15 @@ class AddForiegnKeys extends Migration
      */
     public function up()
     {
+    
         Schema::table('users', function (Blueprint $table) {
+         if(!Schema::hasForeign('users','app_id'))
             $table->foreignId('app_id')->nullable()->constrained("app")->cascadeOnUpdate()->nullOnDelete();;
         });
 
         Schema::table('gift_orders', function (Blueprint $table) {
-            $table->foreignId('app_id')->nullable()->constrained("app")->cascadeOnUpdate()->cascadeOnDelete();;
+            if(!Schema::hasForeign('gift_orders','app_id'));
+                $table->foreignId('app_id')->nullable()->constrained("app")->cascadeOnUpdate()->cascadeOnDelete();;
         });
     }
 
@@ -30,10 +33,12 @@ class AddForiegnKeys extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            if(Schema::hasForeign('users','app_id'));
             $table->dropForeign('app_id');
         });
 
         Schema::table('gift_orders', function (Blueprint $table) {
+            if(Schema::hasForeign('gift_orders','app_id'));
             $table->dropForeign('app_id');
         });
     }
