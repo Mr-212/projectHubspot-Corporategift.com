f2624992901f3284121b2431848f88bd575115ea
Ali Raza <ali.raza@techloyce.com>
2021-04-29 10:58:54 +0500

    updated

--- config/constants.php
+++ config/constants.php
@@ -3,8 +3,12 @@
 return [
 
     'hubspot' => [
-        'client_id' => '3cbf1a7e-914a-4934-9b8b-285dd93fe43b',
-        'client_secret' => '51aedc13-f26b-4984-9b2d-8fd7853af8d4',
+        // 'client_id' => '3cbf1a7e-914a-4934-9b8b-285dd93fe43b',
+        // 'client_secret' => '51aedc13-f26b-4984-9b2d-8fd7853af8d4',
+
+        'client_id' => '12459071-ffa9-40ee-b5e7-5e0b1ebdbc1a',
+        'client_secret' => '929ec6fc-fbd3-48c6-9333-a510fd63d944',
+
         'redirect_uri' => 'https://corporategift.dev-techloyce.com/hupspot-authentication',
         'version' => 'v1',
     ],
