130775deeb097ba99e2ce8d782a9248c7abb41f6
Ali Raza <ali.raza@techloyce.com>
2021-04-30 15:29:50 +0500

    updated constants

--- app/Http/Controllers/HupSpotServiceController.php
+++ app/Http/Controllers/HupSpotServiceController.php
@@ -230,7 +230,7 @@ class HupSpotServiceController extends Controller
      ------------------------------------------------------------------------*/
 
     public function hupspot_data_fetch_request(Request $request){
-       // Log::info($request->headers);
+        Log::info($request->all());
         $identifier = null;
         $app = null;
         if($request->has('userId') && $request->has('portalId')) {
