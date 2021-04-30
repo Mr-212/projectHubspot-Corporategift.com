b3654d0308b05e6262c627215737ecc4c2f8a420
Ali Raza <ali.raza@techloyce.com>
2021-04-30 15:06:14 +0500

    updated constants

--- config/constants.php
+++ config/constants.php
@@ -3,12 +3,14 @@
 return [
 
     'hubspot' => [
-        // 'client_id' => '3cbf1a7e-914a-4934-9b8b-285dd93fe43b',
-        // 'client_secret' => '51aedc13-f26b-4984-9b2d-8fd7853af8d4',
 
-        'client_id' => '12459071-ffa9-40ee-b5e7-5e0b1ebdbc1a',
-        'client_secret' => '929ec6fc-fbd3-48c6-9333-a510fd63d944',
-        'auth_url' => 'https://app.hubspot.com/oauth/authorize?client_id=12459071-ffa9-40ee-b5e7-5e0b1ebdbc1a&redirect_uri=https://corporategift.dev-techloyce.com/hupspot-authentication&scope=contacts%20automation%20tickets',
+        // 'client_id' => '12459071-ffa9-40ee-b5e7-5e0b1ebdbc1a',
+        // 'client_secret' => '929ec6fc-fbd3-48c6-9333-a510fd63d944',
+        // 'auth_url' => 'https://app.hubspot.com/oauth/authorize?client_id=12459071-ffa9-40ee-b5e7-5e0b1ebdbc1a&redirect_uri=https://corporategift.dev-techloyce.com/hupspot-authentication&scope=contacts%20automation%20tickets',
+
+        'client_id' => '339cbfeb-f042-4523-b862-b8bf61647c81',
+        'client_secret' => '0f4f32da-cf89-42ee-b3dd-e0a3e478b370',
+        'auth_url' => 'https://app.hubspot.com/oauth/authorize?client_id=339cbfeb-f042-4523-b862-b8bf61647c81&redirect_uri=https://corporategift.dev-techloyce.com/hupspot-authenticatio&scope=contacts%20content%20automation%20tickets',
 
         'redirect_uri' => 'https://corporategift.dev-techloyce.com/hupspot-authentication',
         'version' => 'v1',
