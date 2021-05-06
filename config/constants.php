870093622ebb1b277e5c76f6470436379a1f4fba
Ali Raza <ali.raza@techloyce.com>
2021-05-06 12:25:38 +0500

    updated /

--- config/constants.php
+++ config/constants.php
@@ -1,11 +1,7 @@
 <?php
 $redirect_url = 'https://corporategift.dev-techloyce.com/hupspot-authentication';
 
-$auth_url = "https://app.hubspot.com/oauth/authorize?client_id=339cbfeb-f042-4523-b862-b8bf61647c81&redirect_uri="
-    .$redirect_url.
-    "&scope=contacts%20timeline%20business-intelligence%20oauth%20forms%20files%20integration-sync%20forms-uploaded-files%20crm.import
-    &optional_scope=content%20reports%20social%20automation%20actions%20transactional-email%20tickets
-    &state=y";
+$auth_url = "https://app.hubspot.com/oauth/authorize?client_id=339cbfeb-f042-4523-b862-b8bf61647c81&redirect_uri=".$redirect_url."&scope=contacts%20timeline%20business-intelligence%20oauth%20forms%20files%20integration-sync%20forms-uploaded-files%20crm.import&optional_scope=content%20reports%20social%20automation%20actions%20transactional-email%20tickets&state=y";
 
 
 return [
