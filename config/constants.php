c1d206eb207fdb17dc065ab5e00dd6a8b37105c0
Ali Raza <ali.raza@techloyce.com>
2021-05-06 10:31:11 +0500

    updated .

--- config/constants.php
+++ config/constants.php
@@ -7,7 +7,7 @@ return [
 
         'client_id' => '339cbfeb-f042-4523-b862-b8bf61647c81',
         'client_secret' => '0f4f32da-cf89-42ee-b3dd-e0a3e478b370',
-        'auth_url' => 'https://app.hubspot.com/oauth/authorize?client_id=339cbfeb-f042-4523-b862-b8bf61647c81&redirect_uri=https://corporategift.dev-techloyce.com/hupspot-authentication&scope=contacts%20content%20automation%20tickets',
+        'auth_url' => 'https://app.hubspot.com/oauth/authorize?client_id=339cbfeb-f042-4523-b862-b8bf61647c81&redirect_uri=https://corporategift.dev-techloyce.com/hupspot-authentication&scope=contacts',
         'redirect_uri' => $redirect_url,
         'version' => 'v1',
         'api_url' =>'https://api.hubapi.com',
